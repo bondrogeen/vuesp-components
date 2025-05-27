b3b76957592d2234dd95bb72bd7539a73af95c4d
Roman Bondarev <bondrogeen@gmail.com>
2025-05-27 21:38:10 +0300

    fix button

--- src/components/general/VButton.vue
+++ src/components/general/VButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="relative flex justify-center items-center transition not-disabled:cursor-pointer" :class="getClass" @click="onClick">
+  <button :class="getClass" @click="onClick">
     <slot></slot>
   </button>
 </template>
@@ -9,12 +9,13 @@ import { defineProps, defineEmits, computed } from 'vue';
 
 interface Props {
   block?: boolean;
+  outline?: boolean;
   color?: string;
   size?: string;
   type?: string;
 }
 
-const { color = '', block, size = 'normal', type = 'button' } = defineProps<Props>();
+const { color = 'gray', block, size = 'normal', type = 'button', outline = false } = defineProps<Props>();
 
 const emit = defineEmits<{
   (e: 'click', value: Event): void;
@@ -22,13 +23,17 @@ const emit = defineEmits<{
 
 const onClick = (e: Event) => emit('click', e);
 
-const colors: { [index: string]: string } = {
-  gray: 'text-gray-500 hover:text-gray-700 bg-white hover:bg-gray-100 dark:border-gray-800 dark:bg-gray-900 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white',
-  blue: 'bg-blue-700 hover:bg-blue-600 text-white border border-blue-700 rounded hover:border-blue-600 disabled:opacity-50 disabled:text-gray-400 disabled:bg-transparent disabled:border-gray-400',
-};
+const gefClass = `relative flex justify-center items-center transition not-disabled:cursor-pointer not-disabled:hover:opacity-90 disabled:opacity-40 rounded`;
+
+const green = outline ? `border border-green-600 text-green-600` : 'bg-green-600 dark:bg-green-800';
+const red = outline ? `border border-red-600 text-red-600` : 'bg-red-600 dark:bg-red-800';
+const gray = outline ? `border border-gray-600 text-gray-600` : 'bg-gray-700 dark:bg-gray-800';
+const blue = outline ? `border border-blue-600 text-blue-600` : 'bg-blue-700 dark:bg-blue-800';
+
+const colors: { [index: string]: string } = { green, red, gray, blue };
 
 const types: { [index: string]: string } = {
-  button: 'font-medium px-4',
+  button: 'px-4',
   icon: 'h-10 w-10 rounded-full border border-gray-200',
 };
 
@@ -38,7 +43,7 @@ const getSize = (size: string): any => sizes?.[size] || '';
 
 const getType = (type: string): any => types?.[type] || '';
 
-const getColor = (color: string): any => colors?.[color] || '';
+const getColor = (color: string): any => `${outline ? 'dark:text-white' : 'text-white'} ${colors?.[color] || ''}`;
 
-const getClass = computed(() => [getColor(color), getType(type), getSize(size), { 'w-full': block }]);
+const getClass = computed(() => [gefClass, getColor(color), getType(type), getSize(size), { 'w-full': block }]);
 </script>
